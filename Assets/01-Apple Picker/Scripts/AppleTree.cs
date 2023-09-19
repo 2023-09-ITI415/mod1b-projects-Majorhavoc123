648901eed0e7414bc267efcd57c892749bb653fa
Matthew Sissick <120825879+Majorhavoc123@users.noreply.github.com>
2023-09-19 15:28:02 -0400

    Apples Drop from Tree Correctly

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -9,27 +9,29 @@ public class AppleTree : MonoBehaviour
 
     // Prefab for instantiating apples
     public GameObject applePrefab;
-
     // Speed at which the AppleTree moves
     public float speed = 1f;
-
     // Distance where AppleTree turns around
     public float leftAndRightEdge = 10f;
-
     // Chance that the AppleTree wull chaange directions
     public float chanceToChangeDirections = 0.1f;
-
     // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrop = 1f;
     
     // Start is called before the first frame update
     void Start()
     {// Dropping apples every second
-        
+        Invoke("DropApple", 2f); // a
+    }
+
+    void DropApple(){ // b
+        GameObject apple = Instantiate<GameObject>(applePrefab); // c
+        apple.transform.position = transform.position; // d
+        Invoke("DropApple", secondsBetweenAppleDrop); // e
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // f
     {
         // Basic Movement
         Vector3 pos = transform.position; // b
