bc52bb4a8c96154f811836233bb3308fd10b74d0
Matthew Sissick <120825879+Majorhavoc123@users.noreply.github.com>
2023-09-27 23:55:39 -0400

    Created Light for Slingshot Interaction

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    public GameObject launchPoint;
+
+    void Awake(){
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -11,11 +18,13 @@ public class Slingshot : MonoBehaviour
     }
 
     void OnMouseEnter(){
-        print("Slingshot:OnMouseEnter()");
+        // print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit(){
-        print("Slingshot:OnMouseExit()");
+        // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 
     // Update is called once per frame
