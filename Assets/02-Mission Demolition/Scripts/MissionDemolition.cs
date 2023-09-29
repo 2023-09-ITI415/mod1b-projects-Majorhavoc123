960b09f24293b564ffcb03a2fe3e76f36452f1cd
Matthew Sissick <120825879+Majorhavoc123@users.noreply.github.com>
2023-09-28 20:40:22 -0400

    End of Class
    Currently making UI elements

---
+++ Assets/02-Mission Demolition/Scripts/MissionDemolition.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode{
+    idle,
+    playing,
+    levelEnd
+}
+
+public class MissionDemolition : MonoBehaviour
+{
+    static private MissionDemolition S;
+
+    [Header("Set in Inspector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Text uitButton;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    [Header("Set Dynamically")]
+    public int level;
+
+
+
+
+
+
+
+
+
+}
