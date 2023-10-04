4b81de2e5fa01ee16414b9fda3a185ce4936c4cb
Matthew Sissick <120825879+Majorhavoc123@users.noreply.github.com>
2023-10-03 23:31:16 -0400

    Finished Game

--- Assets/03-Prototype1/Scripts/PlayerController.cs
+++ Assets/03-Prototype1/Scripts/PlayerController.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
      void OnMove (InputValue movementValue)
@@ -22,6 +29,14 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x; 
         movementY = movementVector.y;
    
+   }
+      void SetCountText() 
+   {
+       countText.text =  "Cheese Collected: " + count.ToString() + " out of 12 needed.";
+        if (count >= 12)
+         {
+           winTextObject.SetActive(true);
+        }
    }
     private void FixedUpdate() 
    {
@@ -35,6 +50,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
        {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
        }
    } 
 }
